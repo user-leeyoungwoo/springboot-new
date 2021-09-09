0f2b40469d7a1cdfcb9c0aead730de879cb05b4a
sangsang <ywlee@koscom.co.kr>
2021-09-09 16:19:51 +0900

    3일차 16시 20분 (제5장 완성)

--- src/test/java/com/koscom/springboot/web/HelloControllerTest.java
+++ src/test/java/com/koscom/springboot/web/HelloControllerTest.java
@@ -1,10 +1,15 @@
 package com.koscom.springboot.web;
 
+import com.koscom.springboot.config.auth.SecurityConfig;
+import com.koscom.springboot.web.HelloController;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,12 +18,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = HelloController.class) // controller 만 테스트 & HelloController 만 테스트
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     MockMvc mvc;
 
+    @WithMockUser(roles = "USER") // MockMvc
     @Test
     void hello주소로요청이오면_hello가_리턴된다() throws Exception {
         String expectResult = "hello";
@@ -28,19 +38,21 @@ public class HelloControllerTest {
                 .andExpect(content().string(expectResult));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
         mvc.perform(get("/hello/dto")
-                .param("name", name)
-                .param("amount", String.valueOf(amount)))
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     void amount가없으면_응답코드가400이_된다() throws Exception {
         String name = "hello";
--- src/test/java/com/koscom/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/koscom/springboot/web/PostsApiControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -24,6 +25,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"security.enabled=false"})
 public class PostsApiControllerTest {
 
     @LocalServerPort
